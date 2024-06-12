b793e30dfe8dffcbe74218b76eadd40374d07d82
Charan Teja B S <cherrypy6@gmail.com>
2024-06-13 04:15:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-// vite.config.js
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/ExpenseTracker/", // Update this to the repository name
-});
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
